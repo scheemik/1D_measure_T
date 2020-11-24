34b554e43d9d5f83cc11b57f496f7aa08beb8716
Grey <mschee@physics.utoronto.ca>
2020-11-24 14:00:56 -0600

    Made scripts to submit jobs to HPC

---
+++ HPC_job_submit.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+# Run this script to submit a job to Niagara
+# Takes in optional arguments:
+#	$ sh HPC_job_submit.sh -e <name of experiment> 				Default: current datetime
+#								  			-s <number of simulations>      Default: 1
+#								  			-c <number of cores>            Default: 1
+#								  			-r <run simulation>             Default: False
+#             	  			-m <merge h5 files>             Default: False
+#								  			-o <post-process data>          Default: False
+#								  			-p <plot simulation>            Default: False
+#             	  			-g <create gif>                 Default: False
+#             	  			-v <create video, mp4>          Default: False
+
+# Current datetime
+DATETIME=`date +"%Y-%m-%d_%Hh%M"`
+
+# Having a ":" after a flag means an option is required to invoke that flag
+while getopts e:s:c:rmopgv option
+do
+	case "${option}"
+		in
+		e) EXP=${OPTARG};;
+		s) SIMS=${OPTARG};;
+		c) CORES=${OPTARG};;
+		r) RUN=r;;
+    m) MER=m;;
+    o) PRO=o;;
+    p) PLT=p;;
+    g) GIF=g;;
+    v) VID=v;;
+	esac
+done
+
+# check to see if arguments were passed
+if [ -z "$NAME" ]
+then
+	EXP=$DATETIME
+	echo "-e, No name specified, using EXP=$EXP"
+else
+  echo "-e, Name specified, using EXP=$EXP"
+fi
+if [ -z "$SIMS" ]
+then
+	SIMS=5
+	echo "-s, No number of simulations specified, using SIMS=$SIMS"
+else
+  echo "-s, Number of simulations specified, using SIMS=$SIMS"
+fi
+if [ -z "$CORES" ]
+then
+	CORES=32
+	echo "-c, No number of cores specified, using CORES=$CORES"
+else
+  echo "-c, Number of cores specified, using CORES=$CORES"
+fi
+if [ "$RUN" = r ]
+then
+	ARGS=true
+	echo "-r, Run the simulation"
+fi
+if [ "$MER" = m ]
+then
+	ARGS=true
+	echo "-m, Merge the data"
+fi
+if [ "$PRO" = o ]
+then
+	ARGS=true
+	echo "-o, Post-process data"
+fi
+if [ "$PLT" = p ]
+then
+	ARGS=true
+	echo "-p, Plot the simulation"
+fi
+if [ "$GIF" = g ]
+then
+	ARGS=true
+	echo "-g, Create gif of simulation"
+fi
+if [ "$VID" = v ]
+then
+	ARGS=true
+	echo "-v, Create video (mp4) of the simulation"
+fi
+
+###############################################################################
+JOBNAME=$EXP
+LANCEUR="HPC_lanceur.slrm"
+###############################################################################
+# Submit job to queue
+if [ "$ARGS" = true ]
+then
+	echo "sbatch --job-name=$JOBNAME $LANCEUR -e $EXP -s $SIMS -c $CORES -$RUN$MER$PRO$PLT$GIF$VID"
+else
+	echo "sbatch --job-name=$JOBNAME $LANCEUR -e $EXP -s $SIMS -c $CORES"
+fi
