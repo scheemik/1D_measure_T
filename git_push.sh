4aa27505ff2668945f83f9d53f9427b71f7908db
Mikhail Schee <mschee@physics.utoronto.ca>
2020-11-30 15:11:33 -0500

    20/11/30 15:11:32 update

---
+++ git_push.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# A script to automate git add, commit, and push
+
+DATE=`date +"%y/%m/%d %T"`
+COMMIT_M="\"$DATE update\""
+
+set -x # echos each command as it is executed
+
+git add --all
+# eval required to avoid trouble with quotation marks
+eval "git commit -m $COMMIT_M"
+git push
