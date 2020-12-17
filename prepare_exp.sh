d23678ecb2d8bf95d206d4682669cd6d043889a2
Grey <mschee@physics.utoronto.ca>
2020-12-17 08:40:00 -0600

    Cleaning up unneeded lines of code

--- prepare_exp.sh
+++ prepare_exp.sh
@@ -3,7 +3,7 @@
 # Run this script to prepare an experiment directory
 # Takes in optional arguments:
 #	$ sh prepare_exp.sh -e <name of experiment> 				 Default: current datetime
-#								  		-s <number of simulations>      Default: 5
+#								  		-s <number of simulations>       Default: 5
 
 echo ""
 echo "Preparing experiment directory"
@@ -38,35 +38,11 @@ else
 fi
 
 ###############################################################################
-# Miscellaneous variables
+# Variables
 
 # Name of experiment plotting file
 plot_exp="plot_exp_data.py"
-
-###############################################################################
-# Code file variables needed for each simulation
-
-# # Name of the run script
-# run_file="run.sh"
-# # Name of the script to write the unique parameters file
-# params_script='write_params.py'
-# # Name of the main code file
-# code_file='main.py'
-# # Name of switchboard file
-# switch_file="switchboard.py"
-# # Name of merging file
-# merge_file="merge.py"
-# # Helper code files
-# helper_funcs="helper_functions.py"
-# helper_funcs_CD="helper_functions_CD.py"
-# # Name of post processing file
-# post_process="post_process.py"
-# # Name of slice plotting file
-# plot_file="plot_slices.py"
-# # Name of gif creation file
-# gif_cre_file="create_gif.py"
-# # Group all the code files for ease of calling
-# CODE_FILES="$run_file $params_script $code_file $switch_file $merge_file $helper_funcs $helper_funcs_CD $post_process $plot_file $gif_cre_file"
+# Name of folder that contains simulation code files
 CODE_FILES="_code_files"
 
 ###############################################################################
@@ -113,10 +89,8 @@ do
 	printf -v NID "%03d" $id
 	# Generate simulation name
 	SIM_NAME="${NID}_${EXP}"
-	# # Make directory for that simulation
-	# mkdir _experiments/${EXP}/${SIM_NAME}
-	# # Copy code files to simulation directory
-	# cp $CODE_FILES _experiments/${EXP}/${SIM_NAME}
+	# Copy code files to experiment directory
 	cp -r $CODE_FILES _experiments/${EXP}
+	# Rename code file folder for specific simulation
 	mv _experiments/${EXP}/${CODE_FILES} _experiments/${EXP}/${SIM_NAME}
 done
