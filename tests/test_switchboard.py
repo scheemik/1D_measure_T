27cc991af4a163872d54a5308ad7b5df451b91bd
Grey <mschee@physics.utoronto.ca>
2021-01-14 16:58:17 -0500

    Having issues importing the switchboard parameters

--- tests/test_switchboard.py
+++ tests/test_switchboard.py
@@ -14,8 +14,21 @@ Each test should do the following:
 3. Compare the output with an expected result
 """
 
+import sys
+sys.path.append("../") # Adds higher directory to python modules path
+import importlib
+switchboard_module = "_code_files.switchboard"
+sbp = importlib.import_module(switchboard_module)
+# sbp = __import__("_code_files/switchboard.py")
+
 def test_always_passes():
     assert True
 
 def test_always_fails():
     assert False
+
+def test_uppercase():
+    assert "loud noises".upper() == "LOUD NOISES"
+
+def test_params():
+    assert sbp.nz == 1024
