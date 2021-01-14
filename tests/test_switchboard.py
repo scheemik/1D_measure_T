86e8ff6b48593dc5b8629b707df34bc1aaff2b8f
Grey <mschee@physics.utoronto.ca>
2021-01-14 13:40:22 -0500

    Created a test file for the switchboard

---
+++ tests/test_switchboard.py
@@ -0,0 +1,7 @@
+# Switchboard test script
+"""
+Description:
+This is a script to test the switchboard for the Dedalus experiment
+It will relations between different values to insure the assumptions needed
+    for the experiment are satisfied
+"""
