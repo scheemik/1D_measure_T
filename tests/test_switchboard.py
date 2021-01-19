f2d6f2a3c31054d863230e9bcf12d8b473b093f7
Grey <mschee@physics.utoronto.ca>
2021-01-19 16:03:32 -0500

    Figured out a relative import for testing

--- tests/test_switchboard.py
+++ tests/test_switchboard.py
@@ -14,21 +14,25 @@ Each test should do the following:
 3. Compare the output with an expected result
 """
 
-import sys
-sys.path.append("../") # Adds higher directory to python modules path
-import importlib
-switchboard_module = "_code_files.switchboard"
-sbp = importlib.import_module(switchboard_module)
+import os, sys
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
+from _code_files import switchboard as sbp
+# sys.path.append("../") # Adds higher directory to python modules path
+# import importlib
+# switchboard_module = "_code_files.switchboard"
+# sbp = importlib.import_module(switchboard_module)
 # sbp = __import__("_code_files/switchboard.py")
 
 def test_always_passes():
     assert True
 
-def test_always_fails():
-    assert False
-
-def test_uppercase():
-    assert "loud noises".upper() == "LOUD NOISES"
+# def test_always_fails():
+#     assert False
 
 def test_params():
+    assert sbp.mL == 2.0
+
+def test_switchboard():
     assert sbp.nz == 1024
