b6bb930bd747a71a14a754f636c45a5196ab352b
GreyAudron <mschee@physics.utoronto.ca>
2021-04-22 13:24:42 -0400

    Made plot of 4 layer T vs kL in dark mode

--- _experiments/pres_64s_many_l/plot_many_exp_data.py
+++ _experiments/pres_64s_many_l/plot_many_exp_data.py
@@ -2,23 +2,13 @@
 
 This script plots the simulated and analytical values of the transmission coefficient
 
-Usage:
-    plot_exp_data.py EXP SIMS
-
-Options:
-    EXP             # name of the experiment from -e
-    SIMS            # number of simulations from -s
-
 """
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Parse input parameters
-from docopt import docopt
-args = docopt(__doc__)
-exp_name    = args['EXP']       # Experiment name, needed to find all csv files
-num_sims    = int(args['SIMS']) # Number of simulations in the experiment
+# Enable dark mode plotting
+plt.style.use('dark_background')
 
 ###############################################################################
 # Import relevant modules
@@ -39,6 +29,7 @@ omega   = 1 * np.cos(theta)
 ###############################################################################
 # Read data from the csv file
 csv_files = ["1l_exp_data.csv", "2l_exp_data.csv", "3l_exp_data.csv", "4l_exp_data.csv"]
+num_sims = 64
 
 def combine_exp_csv_files(csv_files, output_file):
     import csv
@@ -77,28 +68,37 @@ def plot_T_vs_kL(all_exp_data, theta, omega, title_str='Transmission Coefficient
     theta           Angle at which wave is incident on stratification structure
     omega           frequency of wave
     """
+    this_font_size = 16
     # Set figure and axes for plot, fig_ratio=1 makes a square
-    fig, axes = hf.set_fig_axes([1], [1], fig_ratio=0.8)
+    fig, axes = hf.set_fig_axes([1], [1], fig_ratio=0.6)
     # Create array of analytical function
     kL_ana = np.linspace(all_exp_data[0,0], all_exp_data[-1,0], 100)
     ana_data = hf.SY_eq2_4(theta, kL_ana)
     # Plot line for analytical solution
-    axes.plot(kL_ana, ana_data, color='black', label=r'$\mathbb{T}_{ana}$')
+    # axes.plot(kL_ana, ana_data, color='black', label=r'$\mathbb{T}_{ana}$')
     # Get colors
     plt_clrs = [hf.CSS4_COLORS['forestgreen'],
                 hf.CSS4_COLORS['blueviolet'],
                 hf.CSS4_COLORS['tomato'],
                 hf.CSS4_COLORS['royalblue']]
+    labels   = [rf'$1$ layer',
+                rf'$2$ layers',
+                rf'$3$ layers',
+                rf'$4$ layers']
+    markers  = ['o',
+                'v',
+                '^',
+                'x']
     # Plot points from measurements in simulations
     for i in range(1,all_exp_data.shape[1]):
-        axes.plot(all_exp_data[:,0], all_exp_data[:,i], color=plt_clrs[i-1], marker='.', label=rf'$T_{i}$')
-    axes.legend()
+        axes.plot(all_exp_data[:,0], all_exp_data[:,i], color=plt_clrs[i-1], marker=markers[i-1], markersize=5, label=labels[i-1])#rf'$T_{i}$')
+    axes.legend(fontsize=this_font_size-2)
     # Add labels and titles
-    axes.set_xlabel(r'$kL$')
+    axes.set_xlabel(r'$k_xL$', fontsize=this_font_size)
     axes.set_xlim(0,2.5)
-    axes.set_ylabel(r'$\mathbb{T}$')
-    plt.title(r'%s for $\theta=$%s' %(title_str, hf.rad_to_degs(theta)))
+    axes.set_ylabel(r'T', fontsize=this_font_size+6)
+    plt.title(r'%s for $\theta=$%s' %(title_str, hf.rad_to_degs(theta)), fontsize=this_font_size)
     #plt.show()
-    plt.savefig(filename)
+    plt.savefig(filename, dpi=300)
 
 plot_T_vs_kL(all_exp_data, theta, omega)
